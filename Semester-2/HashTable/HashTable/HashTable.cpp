5e53d0b52903f2b1794d2afe53ae6f536152fe93
ACC-Stas <79222536+ACC-Stas@users.noreply.github.com>
2021-04-04 21:21:09 +0300

    create HashTable

---
+++ Semester-2/HashTable/HashTable/HashTable.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include "Header.h"
+
+int main() {
+	HashTable<int, int> hs;
+	std::cout << "Testing insert and rehash\n";
+	hs.insert(5, 5);
+	try {
+		hs.insert(5, 6);
+	}
+	catch (std::exception& ex) {
+		std::cout << ex.what();
+	}
+	hs.insert(6, 10);
+	hs.insert(10, 8);
+	std::cout << hs.toString() << '\n';
+	std::cout << "\nTesting erase\n";
+	hs.erase(10);
+	std::cout << hs.toString() << '\n';
+	hs.erase(0);
+	std::cout << hs.toString() << '\n';
+	hs.erase(6);
+	std::cout << hs.toString() << '\n';
+	hs.erase(5);
+	std::cout << hs.toString() << '\n';
+	std::cout << "Testing insert after erase\n";
+	hs.insert(18, 25);
+	std::cout << hs.toString() << '\n';
+	hs.insert(145, 14);
+	std::cout << hs.toString() << '\n';
+	hs.insert(6, 10);
+	hs.insert(10, 8);
+	hs.insert(5, 5);
+	std::cout << hs.toString() << '\n';
+	std::cout << "Testing clear\n";
+	hs.clear();
+	std::cout << hs.toString() << '\n';
+	hs.insert(15, 15);
+	hs.insert(10, 20);
+	hs.insert(145, 25);
+	hs.insert(0, 3);
+	std::cout << hs.toString() << '\n';
+	std::cout << "Testing containts\n";
+	std::cout << hs.contains(145) << '\n';
+	std::cout << hs.contains(66) << '\n';
+	std::cout << "Testing operator =\n";
+	HashTable<int, int> hs2;
+	hs2 = hs;
+	std::cout << hs2.toString() << '\n';
+	std::cout << "Testing copy constructor\n";
+	HashTable<int, int> hs3(hs2);
+	std::cout << hs3.toString() << '\n';
+	std::cout << "Testing operator[]\n";
+	std::cout << hs[0] << '\n';
+	std::cout << hs[8] << '\n';
+	hs[8] = 15;
+	std::cout << hs[8] << '\n';
+	hs[202] = 43;
+	std::cout << hs[202] << '\n';
+}
---
+++ Semester-2/HashTable/HashTable/Header.h
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <forward_list>
+#include <string>
+
+template <class KeyType, class T, class Hasher = std::hash<KeyType>>
+class HashTable {
+private:
+	Hasher hash;
+	size_t size;
+	const size_t maxBatchSize;
+	std::forward_list<std::pair<const KeyType, T>>* elements;
+public:
+	HashTable(size_t size = 1, size_t maxBatchSize = 2) : maxBatchSize(maxBatchSize) {
+		if (maxBatchSize == 0) {
+			throw std::exception("Batch size can't be 0\n");
+		}
+		if (size == 0) {
+			throw std::exception("Size can't be 0\n");
+		}
+		this->size = size;
+		elements = new std::forward_list<std::pair<const KeyType, T>>[size];
+		for (int i = 0; i < size; i++) {
+			elements[i] = std::forward_list<std::pair<const KeyType, T>>();
+		}
+	}
+	HashTable(const HashTable& ht) : maxBatchSize(ht.maxBatchSize) {
+		this->operator=(ht);
+	}
+	HashTable& operator=(const HashTable& ht) {
+		size = ht.size;
+		elements = new std::forward_list<std::pair<const KeyType, T>>[size];
+		for (int i = 0; i < size; i++) {
+			elements[i] = std::forward_list<std::pair<const KeyType, T>>();
+		}
+		for (int i = 0; i < ht.size; i++) {
+			for (auto it : ht.elements[i]) {
+				insert(it.first, it.second);
+			}
+		}
+		return *this;
+	}
+	T& operator[](const KeyType key) {
+		int position = hash(key) % size;
+		for (auto it = elements[position].begin(); it != elements[position].end(); it++) {
+			if ((*it).first == key) {
+				return (*it).second;
+			}
+		}
+		insert(key, T());
+		for (auto it = elements[position].begin(); it != elements[position].end(); it++) {
+			if ((*it).first == key) {
+				return (*it).second;
+			}
+		}
+	}
+	void insert(const KeyType key, T value) {
+		int position = hash(key) % size;
+		int counter = 0;
+		for (auto it : elements[position]) {
+			if (it.first == key) {
+				throw std::exception("Already have such key\n");
+			}
+			counter++;
+		}
+		elements[position].push_front(std::pair<const KeyType, T>(key, value));
+		counter++;
+		if (counter > maxBatchSize) {
+			rehash(2 * size);
+		}
+	}
+	void rehash(size_t newSize) {
+		if (newSize == 0) {
+			throw std::exception("Size can't be 0\n");
+		}
+		std::forward_list<std::pair<const KeyType, T>>* oldelements = elements;
+		elements = new std::forward_list<std::pair<const KeyType, T>>[newSize];
+		for (int i = 0; i < newSize; i++) {
+			elements[i] = std::forward_list<std::pair<const KeyType, T>>();
+		}
+		int oldSize = size;
+		size = newSize;
+		for (int i = 0; i < oldSize; i++) {
+			for (auto it : oldelements[i]) {
+				insert(it.first, it.second);
+			}
+		}
+		delete[] oldelements;
+	}
+	void erase(const KeyType key) {
+		int position = hash(key) % size;
+		auto previousIt = elements[position].end();
+		for (auto it = elements[position].begin(); it != elements[position].end(); it++) {
+			if ((*it).first == key) {
+				if (previousIt != elements[position].end()) {
+					elements[position].erase_after(previousIt);
+					break;
+				}
+				else {
+					elements[position].pop_front();
+					break;
+				}
+			}
+			previousIt = it;
+		}
+	}
+	std::string toString() { //used for debug
+		std::string b;
+		for (int i = 0; i < size; i++) {
+			for (auto it : elements[i]) {
+				b += "first = " + std::to_string(it.first) + " second = " + std::to_string(it.second) + ", ";
+			}
+		}
+		return b;
+	}
+	bool contains(KeyType key) {
+		bool find = false;
+		int position = hash(key) % size;
+		for (auto it : elements[position]) {
+			if (it.first == key) {
+				find = true;
+			}
+		}
+		return find;
+	}
+	void clear() {
+		delete[] elements;
+		size = 1;
+		elements = new std::forward_list<std::pair<const KeyType, T>>[size];
+	}
+	~HashTable() {
+		delete[] elements;
+	}
+};
